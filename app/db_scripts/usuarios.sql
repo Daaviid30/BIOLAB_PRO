70021896cb3b3a6723ee22499577b26c86219325
Daaviid30 <davidmartinc30@gmail.com>
2023-10-22 13:03:15 +0200

    primer commit

---
+++ app/db_scripts/usuarios.sql
@@ -0,0 +1,18 @@
+CREATE TABLE usuarios(
+    dni CHAR(9) PRIMARY KEY NOT NULL,
+    nombre VARCHAR(30) NOT NULL,
+    apellidos VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    telefono CHAR(9) NOT NULL,
+    salt BLOB NOT NULL,
+    encryped_pass BLOB NOT NULL
+);
+
+
+CREATE TABLE papers(
+    dni CHAR(9) NOT NULL,
+    titulo VARCHAR(150) NOT NULL,
+    cuerpo VARCHAR(10000) NOT NULL,
+    PRIMARY KEY (dni,titulo),
+    FOREIGN KEY (dni) REFERENCES usuarios(dni)
+);
---
+++ app/main.py
@@ -0,0 +1,69 @@
+"""Proyecto de criptografía
+Autores: David Martín (100472099) / Iván Llorente (100472242)"""
+
+#Importamos las librerias necesarias para la ejecución de la app
+import inicio_sesion
+import registro
+from flask import Flask, request, render_template
+
+# Creamos la aplicación Flask
+app = Flask(__name__)
+
+# La ruta de inicio de la app será la pagina de log-in
+@app.route('/')
+def inicio():
+    return render_template('login.html')
+
+# La ruta /formulario dentro de la app llevará al manejo de datos del panel de log-in
+@app.route('/formulario', methods=['POST'])
+def procesar():
+    dni = request.form['DNI']
+    password = request.form['password']
+    
+    mensaje_estado = inicio_sesion.login_usuario(dni, password)
+    return render_template('login.html', msg=mensaje_estado[0], msg_class=mensaje_estado[1])
+
+# La ruta /sing-up te lleva a la pagina de sing-up
+@app.route('/sing-up')
+def sing_up():
+    return render_template('singup.html')
+
+
+# La ruta /form-singup maneja los datos del formulario de registro
+@app.route('/form-singup', methods=['POST'])
+def form_singup():
+    dni = request.form['DNI']
+    name = request.form['name']
+    surname = request.form['surname']
+    email = request.form['email']
+    phone = request.form['phone']
+    password = request.form['password']
+    password2 = request.form['password2']
+
+    mensaje_estado = registro.registro_usuario(dni, name, surname, email, phone, password, password2)
+    return render_template('singup.html', msg=mensaje_estado[0], msg_class=mensaje_estado[1])
+
+
+@app.route('/segundo_factor')
+def segundo_factor():
+    return render_template('segundo_factor.html')
+
+@app.route('/form-2fa', methods=['POST'])
+def form_2fa():
+    codigo_verificacion = request.form['codigo']
+
+    mensaje_estado = inicio_sesion.comprobar_codigo_verificación(codigo_verificacion)
+    return render_template('segundo_factor.html', msg=mensaje_estado[0], msg_class=mensaje_estado[1])
+
+@app.route('/principal')
+def principal():
+    return render_template('principal.html')
+
+@app.route('/form-principal', methods=['POST'])
+def form_principal():
+    titulo = request.form['titulo']
+    cuerpo = request.form['cuerpo']
+
+# Ejecución del programa
+if __name__ == '__main__':
+    app.run(debug=True)

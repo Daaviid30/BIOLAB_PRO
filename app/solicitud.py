863b2bfa745394b602fb39cb5fe4d947073b4dd4
ivaanlc27 <100472242@gmail.com>
2023-11-27 16:34:51 +0100

    si es doctor y solicita, le dice que ya es doctor

--- app/solicitud.py
+++ app/solicitud.py
@@ -11,17 +11,30 @@ def recuperar():
 
 def anadir_solicitud():
     user_name = pasar_identificador()
+
+    # Comprobar si ya tiene permiso de doctor
+    consulta = "SELECT permiso FROM usuarios WHERE user_name = %s"
+    values = (user_name,)
+    conexion_db.cursor.execute(consulta, values)
+    permiso = conexion_db.cursor.fetchall()[0][0]
+    if permiso[0] == 'D':
+        return "Ya tienes permisos de doctor", "error"
+    
+
     consulta = "SELECT user_name, rechazada FROM solicitudes WHERE user_name = %s"
     values = (user_name,)
     conexion_db.cursor.execute(consulta, values)
     resultado = conexion_db.cursor.fetchall()
     
+    # Comprobar si ya ha solicitado permiso
     if len(resultado) != 0:
+        # Comprobar si la solicitud fue rechazada
         if resultado[0][1] == 1:
             mensaje = "Su solicitud fue rechazada previamente. Contacte con un administrador"
         else:    
             mensaje = "Ya has solicitado permiso de doctor"
         estado = "error"
+    # Si no ha solicitado permiso, se añade a la base de datos    
     else:
         consulta = "INSERT INTO solicitudes (user_name, rechazada) VALUES (%s, %s)"
         values = (user_name, 0,)
